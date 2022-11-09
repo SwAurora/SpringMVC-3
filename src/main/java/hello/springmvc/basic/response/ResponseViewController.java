fd495deb51e0d523e4069a15385b99d45eb4df2f
SwAurora <rlatjddnr74@gmail.com>
2022-11-09 16:06:05 +0900

    2022-11-09 master(ver2)

--- src/main/java/hello/springmvc/basic/response/ResponseViewController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -1,9 +1,12 @@
 package hello.springmvc.basic.response;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @Controller
 public class ResponseViewController
 {
@@ -11,5 +14,18 @@ public class ResponseViewController
     public ModelAndView responseViewV1()
     {
         return new ModelAndView("response/hello").addObject("data", "hello!");
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model)
+    {
+        model.addAttribute("data", "hello!");
+        return "response/hello";
+    }
+
+    @RequestMapping("/response/hello")
+    public void responseViewV3(Model model)
+    {
+        model.addAttribute("data", "hello!");
     }
 }
